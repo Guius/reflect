f079862a083f6728f040304a1d0d25ac9b3b7e84
Guius <42870822+Guius@users.noreply.github.com>
2019-05-02 18:24:45 +0100

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,35 @@
+<?php
+include('connection.php');
+if($_POST['submit']) {
+    
+
+    $id = $_POST['ID'];
+
+    $sql = "DELETE FROM newaction WHERE id='$id'"; 
+
+
+    if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully <br><br>";
+    echo "<a href='onlinehappiness.php'>Return to main page</a>";
+} else {
+    echo "Error deleting record: " . $conn->error;
+} 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// echo $rowID + $numberRand;
+
+?>
